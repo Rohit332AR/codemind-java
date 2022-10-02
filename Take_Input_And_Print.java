0d8b4afae223d3c03c498aede05b46fa5a66b883
Rohit <98220108+Rohit332AR@users.noreply.github.com>
2022-10-02 18:32:04 +0530

    import java.util.Scanner; class Print{ 	public static void main(String[] args) { 		Scanner in=new Scanner(System.in); 		String hello=in.nextLine(); 		System.out.println(hello);
    	}
    }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Print{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		String hello=in.nextLine();
+		System.out.println(hello);
+	   
+	}
+}
