070cc19adfea10e369bf726b992e1d731cee8240
Rohit <98220108+Rohit332AR@users.noreply.github.com>
2022-10-06 19:30:23 +0530

    import java.util.Scanner; class Pattern{ public static void main(String[] args){          Scanner in=new Scanner(System.in);          int n=in.nextInt();          for(int i=n;i>=1;i--){              for(int j=1;j<=n;j++){                  if(j==i || j==n-i+1){                      System.out.print("x");                  }                  else{                      System.out.print("0");                  }
                 }
                 System.out.println();
             }
             
             }
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 class Pattern{
 public static void main(String[] args){
          Scanner in=new Scanner(System.in);
-          int n=in.nextInt();
-          for(int i=1;i<=n;i++){
-              for(int j=1;j<=n;j++){
-                  if(j==i){
-                      System.out.print("0");
-                  }
-                  else{
-                      System.out.print("x");
-                  }
-              }
-              System.out.println();
-          }
+         int n=in.nextInt();
+         for(int i=n;i>=1;i--){
+             for(int j=1;j<=n;j++){
+                 if(j==i || j==n-i+1){
+                     System.out.print("x");
+                 }
+                 else{
+                     System.out.print("0");
+                 }
+                 
+             }
+             System.out.println();
+         }
+         
          }
 }
